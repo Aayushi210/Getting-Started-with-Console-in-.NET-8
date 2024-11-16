fd988b19e6d80ceaa54b8837dad8c7dfbb557440
MD HASAN MONSUR <hasanmcse@gmail.com>
2024-11-16 11:27:53 +0600

    hasan

---
+++ MyConsoleApp/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Welcome to My Console App in .NET 8!");
+Console.Write("Enter your name: ");
+string name = Console.ReadLine();
+Console.WriteLine($"Hello, {name}! Enjoy coding in .NET 8!");
